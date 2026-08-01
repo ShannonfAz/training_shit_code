e2b4d9d889079ebee57285f740d5c68b98b75cfb
Shannon_fAz <2783965406@qq.com>
2026-08-01 20:51:42 +0800

    删库

--- feat_stats.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-data = np.load('feat_stats.npz')
-mean = data['mean']  # shape (5,)
-std  = data['std']   # shape (5,)
-
-print("// 请将以下内容复制到 grurun.cpp 中替换 FEAT_MEAN 和 FEAT_STD")
-print("static const float FEAT_MEAN[5] = {", ", ".join(f"{v:.8f}f" for v in mean), "};")
-print("static const float FEAT_STD[5]  = {", ", ".join(f"{v:.8f}f" for v in std),  "};")
